fda3e4e4cded6b19319ca605b251364bcbc0faee
Trevor <tjc1295@gmail.com>
2024-01-10 14:46:25 -0500

    JSON views/Aggragate refactoring

--- presentations/api_views.py
+++ presentations/api_views.py
@@ -1,7 +1,17 @@
 from django.http import JsonResponse
-
 from .models import Presentation
+from common.json import ModelEncoder
 
+class PresentationDetailEncoder(ModelEncoder):
+    model = Presentation
+    properties = [
+        "presenter_name",
+        "company_name",
+        "presenter_email",
+        "title",
+        "synopsis",
+        "created",
+    ]
 
 def api_list_presentations(request, conference_id):
     """
@@ -46,19 +56,11 @@ def api_show_presentation(request, id):
     the presentation, the synopsis of the presentation, when
     the presentation record was created, its status name, and
     a dictionary that has the conference name and its URL
-
-    {
-        "presenter_name": the name of the presenter,
-        "company_name": the name of the presenter's company,
-        "presenter_email": the email address of the presenter,
-        "title": the title of the presentation,
-        "synopsis": the synopsis for the presentation,
-        "created": the date/time when the record was created,
-        "status": the name of the status for the presentation,
-        "conference": {
-            "name": the name of the conference,
-            "href": the URL to the conference,
-        }
-    }
     """
-    return JsonResponse({})
+
+    presentation = Presentation.objects.get(id=id)
+    return JsonResponse(
+        presentation,
+        encoder=PresentationDetailEncoder,
+        safe=False,
+    )
